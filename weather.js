d6c21117e5f29c408bdb4697dbfb7823d7fb9b1b
andresiturria <andresiturria96@gmail.com>
2022-05-31 15:31:23 +0200

    made the api call a function

--- weather.js
+++ weather.js
@@ -2,11 +2,21 @@ require('dotenv').config()
 const axios = require('axios');
 const API_KEY = process.env.API_KEY
 
-axios.get('http://api.weatherapi.com/v1/current.json', { params: { key: API_KEY, q: "Madrid" } })
-    .then(res => {
-        console.log(res.data);
-    })
-    .catch(error => {
+async function test(){
+
+    try{
+        const res = await axios.get('http://api.weatherapi.com/v1/current.json',
+            { params: { key: API_KEY, q: "Madrid" } })
+        return(res.data)
+    } catch (error) {
         console.error(error);
-    });
+    }
+}
+
+test().then(function (response) {
+    // handle success
+    console.log(response);
+})
+
+
 
