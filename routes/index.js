8b55ba8b2853350b1b6d93de30af27dc37087636
José Manuel Martínez <josemanuel@MacBook-Pro-de-Jose.local>
2022-06-10 22:39:34 +0200

    Template Drivers

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.route('/').get(function(req, res, next){
+    res.render('index.njk')
+})
 
 module.exports = router;
