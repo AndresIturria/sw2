bcab2068becd5a598ddb48ed09cd040c0f9e9b36
Ubuntu <ubuntu@ip-172-31-40-252.eu-west-3.compute.internal>
2022-06-11 21:28:59 +0000

    Cambios Njk

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.route('/').get(function(req, res, next){
-    res.render('index.njk')
+router.route('/')
+	.get(function(req, res, next){
+    		console.log(req.query)
+		res.render('index.njk')
 })
 
 module.exports = router;
