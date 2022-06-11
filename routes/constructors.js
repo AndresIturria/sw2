beef78814a17d2c8bfe4d437acddb0aec852b404
andresiturria <andresiturria96@gmail.com>
2022-06-11 15:07:29 +0200

    get de constructors.js

--- routes/constructors.js
+++ routes/constructors.js
@@ -1,13 +1,41 @@
 var express = require('express');
 const Constructor = require("../models/constructor");
 var router = express.Router();
-var xml2js = require('xml2js');
+const { toXML } = require('jstoxml');
+var xpath = require('xpath')
+var dom = require('xmldom').DOMParser
+
+function json_to_xml(input){
+  let xml = "";
+
+  const prefixXML = `<?xml version="1.0" encoding="UTF-8"?>\n<constructors>\n`;
+  const suffixXML = "\n</constructors>";
+
+  const keys = ["constructorId", "constructorRef", "name", "nationality"]
+
+  input.forEach((constructor) => {
+    let valueXML = keys
+        .map((key) => {
+          return `<${key}>${constructor[key]}</${key}>`;
+        })
+        .join("\n\t");
+    xml += `
+    <constructor>
+      ${valueXML}
+    </constructor>
+  `;
+  });
+
+  const final = prefixXML + xml + suffixXML;
+
+  return(final);
+}
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   Constructor.find({}, {_id: 0}, function (err, constructors){
-    var builder = new xml2js.Builder();
-    var xml = builder.buildObject(constructors);
+    xml = json_to_xml(constructors)
     res.send(xml)
   });
 });
