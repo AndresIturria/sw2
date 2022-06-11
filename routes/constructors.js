6ae0985efd28347b302061523015e5b2e3cb0cb2
andresiturria <andresiturria96@gmail.com>
2022-06-11 13:56:24 +0200

    instalada libreria para xml y corregida ruta de races

--- routes/constructors.js
+++ routes/constructors.js
@@ -1,11 +1,14 @@
 var express = require('express');
 const Constructor = require("../models/constructor");
 var router = express.Router();
+var xml2js = require('xml2js');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   Constructor.find({}, {_id: 0}, function (err, constructors){
-    res.json(constructors)
+    var builder = new xml2js.Builder();
+    var xml = builder.buildObject(constructors);
+    res.send(xml)
   });
 });
 
--- routes/races.js
+++ routes/races.js
@@ -5,10 +5,8 @@ var router = express.Router();
 //rutas tienen que ir de la más específica a la menos.
 
 //getRaceById
-router.get('/id', function(req, res, next) {
-  console.log("aqui")
-  console.log(req.query.raceId)
-  Race.find({raceId: req.query.raceId.toString()}, {_id: 0}, function (err, races){
+router.get('/:raceId', function(req, res, next) {
+  Race.find({raceId: req.params.raceId.toString()}, {_id: 0}, function (err, races){
     console.log(races)
     res.json(races)
   });
@@ -16,8 +14,6 @@ router.get('/id', function(req, res, next) {
 
 //getRaces
 router.get('/', function(req, res, next) {
-  console.log("aqui no")
-  console.log(req.query.raceId)
   Race.find({}, {_id: 0}, function (err, races){
     res.json(races)
   });
