294b24d50e5b24e1c718227b81b383770d4811eb
andresiturria <andresiturria96@gmail.com>
2022-06-12 22:02:38 +0200

    terminado todo falta testing

--- routes/get_constructors_front.js
+++ routes/get_constructors_front.js
@@ -1,63 +1,53 @@
 var express = require('express');
 var router = express.Router();
 const axios = require('axios');
+const Constructor = require("../models/constructor");
+const Circuit = require("../models/circuit");
 
 /* GET users listing. */
 router.post('/', async function (req, res, next) {
 // constructors-get ---------------------------------------------------------------------------
     if (req.body.formInstance == "get") {
         if (req.body.constructorId == ""){
-            try {
-                const consulta = await axios.get('http://localhost:3000/constructors')
-                constructors = consulta.data
+            Constructor.find({}, {_id: 0}, function (err, constructors){
                 res.render('get_constructors_front.njk', {data: constructors});
-            } catch (error) {
-                console.error(error);
-            }
+            });
         }
         // con id ---------------------------------------------
         else if (req.body.constructorId){
-            try {
-                let urlString = `http://localhost:3000/constructors/${req.body.constructorId}`
-                const consulta = await axios.get(urlString)
-                constructors = consulta.data
+            Constructor.find({constructorId: req.body.constructorId}, {_id: 0}, function (err, constructors){
                 res.render('get_constructors_front.njk', {data: constructors});
-            } catch (error) {
-                console.error(error);
-            }
+            });
+
         }
     }
     else if (req.body.formInstance == "post"){ //--------post ------------------------------------
-        try {
-            let urlString = `http://localhost:3000//`
-            raceJson = {
-                year: req.body.year,
-                round: req.body.round,
-                circuitId: req.body.circuitId,
-                name: req.body.name,
-                date: req.body.date,
-                time: req.body.time
-            };
-            const consulta = await axios.post(urlString, raceJson)
-            res.send("<p>Race added</p> </p><a href='/races_front'>Go Back</a>");
-        } catch (error) {
-            console.error(error);
-        }
+        Constructor.findOne().sort({constructorId: -1}).exec(function (err, result){
+            if (err){
+                res.render('error.njk');
+            }
+            let constructorId = result.constructorId + 1;
+            const newConstructor = new Constructor();
+            newConstructor.constructorId = constructorId;
+            newConstructor.constructorRef = req.body.constructorRef;
+            newConstructor.name = req.body.name;
+            newConstructor.nationality = req.body.nationality;
+            newConstructor.save();
+            res.send("<p>Constructor updated<p></p><a href='/constructors_front'>Go Back</a>");
+
+        });
 
     }
-    else if(req.body.formInstance == "put"){ // ----------------put---------------------------------
-        let urlString = `http://localhost:3000/races/${req.body.raceId}`
-        raceJson = {
-            year: req.body.year,
-            round: req.body.round,
-            circuitId: req.body.circuitId,
+    else if (req.body.formInstance == "put"){ // ----------------put---------------------------------
+        let urlString = `http://localhost:3000/constructors/${req.body.raceId}`
+        let constructorJson = {
+            constructorRef: req.body.constructorRef,
             name: req.body.name,
-            date: req.body.date,
-            time: req.body.time
+            nationality: req.body.nationality
         };
         try {
-            const consulta = await axios.put(urlString, raceJson)
-            res.send("<p>Race updated<p></p><a href='/races_front'>Go Back</a>");
+            const consulta = await axios.put(urlString, constructorJson)
+            res.send("<p>Constructor updated<p></p><a href='/races_front'>Go Back</a>");
         } catch (error) {
             console.error(error);
         }
